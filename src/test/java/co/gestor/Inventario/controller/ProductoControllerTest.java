9316428b85346e7a807108f687c584ca67e963a9
Santiago Barrera <santiagobasa@unisabana.edu.co>
2023-09-20 18:22:56 -0500

    Creacion de archivo CI.yml

---
+++ src/test/java/co/gestor/Inventario/controller/ProductoControllerTest.java
@@ -0,0 +1,23 @@
+package co.gestor.Inventario.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductoControllerTest {
+
+    @Test
+    void guardarProducto() {
+        System.out.println("Prueba exitosa de guardar producto");
+    }
+
+    @Test
+    void actualizarProducto() {
+        System.out.println("Prueba exitosa de actualizar producto");
+    }
+
+    @Test
+    void eliminarProducto() {
+        System.out.println("Prueba exitosa de eliminar producto");
+    }
+}
---
+++ src/test/java/co/gestor/Inventario/logica/ProductoLogicaTest.java
@@ -0,0 +1,25 @@
+package co.gestor.Inventario.logica;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductoLogicaTest {
+
+    @Test
+    void guardarProducto() {
+        System.out.println("Prueba exitosa");
+    }
+
+    @Test
+    void actualizarProducto() {
+        System.out.println("Prueba exitosa");
+    }
+
+    @Test
+    void eliminarProducto() {
+        System.out.println("Prueba exitosa");
+
+    }
+
+}
