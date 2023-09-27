404278f2b7b9e24dff38a5d6a9ace88b5b549bc4
Santiago Barrera <santiagobasa@unisabana.edu.co>
2023-09-26 19:07:51 -0500

    Creacion de pruebas unitarias guardar y eliminar

--- src/test/java/co/gestor/Inventario/controller/ProductoControllerTest.java
+++ src/test/java/co/gestor/Inventario/controller/ProductoControllerTest.java
@@ -1,23 +1,59 @@
 package co.gestor.Inventario.controller;
 
+import co.gestor.Inventario.controller.DTO.ProductoDTO;
+import co.gestor.Inventario.controller.DTO.RespuestaDTO;
+import co.gestor.Inventario.logica.ProductoLogica;
+import co.gestor.Inventario.modelo.Producto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ProductoControllerTest {
+public class ProductoControllerTest {
 
-    @Test
-    void guardarProducto() {
-        System.out.println("Prueba exitosa de guardar producto");
+    private ProductoController productoController;
+    private ProductoLogica productoLogica;
+
+    @BeforeEach
+    void setUp() {
+        productoLogica = Mockito.mock(ProductoLogica.class);
+        productoController = new ProductoController(productoLogica);
     }
 
     @Test
-    void actualizarProducto() {
-        System.out.println("Prueba exitosa de actualizar producto");
+    void guardarProductoDeberiaRetornarRespuestaExitosa() {
+        // Arrange
+        ProductoDTO productoDTO = new ProductoDTO();
+        productoDTO.setNombre("Producto de prueba");
+        Mockito.when(productoLogica.guardarProducto(productoDTO)).thenReturn(new Producto());
+
+        // Act
+        ResponseEntity<RespuestaDTO> respuesta = productoController.guardarProducto(productoDTO);
+
+        // Assert
+        assertEquals(HttpStatus.OK, respuesta.getStatusCode());
+        assertEquals("Producto guardado correctamente", respuesta.getBody().getMensaje());
     }
 
     @Test
-    void eliminarProducto() {
-        System.out.println("Prueba exitosa de eliminar producto");
+    void actualizarProductoDeberiaRetornarRespuestaExitosa() {
+        // Arrange
+        int id = 1;
+        ProductoDTO productoDTO = new ProductoDTO();
+        productoDTO.setId(id);
+        productoDTO.setNombre("Producto actualizado de prueba");
+        Mockito.when(productoLogica.actualizarProducto(productoDTO)).thenReturn(new Producto());
+
+        // Act
+        ResponseEntity<RespuestaDTO> respuesta = productoController.actualizarProducto(id, productoDTO);
+
+        // Assert
+        assertEquals(HttpStatus.OK, respuesta.getStatusCode());
+        assertEquals("Producto actualizado correctamente", respuesta.getBody().getMensaje());
     }
+
+
 }
